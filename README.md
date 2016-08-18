692dfcc93f58860c1ac6e4b91c4c8f8a02ecf384
Pudding <philip.strecker@googlemail.com>
2016-08-18 22:34:02 +0200

    added own Predicate interface and Predicates. Added examples to README

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # Java8 Predicates
 
 The intention of this project is to improve readability of equality and relational operators (< > <= >= == !=) using predefined [Predicates](https://docs.oracle.com/javase/8/docs/api/java/util/function/Predicate.html).
+It also contains a Predicate interface with then(), orElse() and elseIf() to replace if-then and if-then-else statements.
 
 ----------
 
 ## Predicates
-- **isEqualTo** | a == b
-- **isNotEqualTo** | a != b
-- **isBiggerThan** | a > b
-- **isSmallerThan** | a < b
-- **isEqualOrBiggerThan** | a >= b
-- **isEqualOrSmallerThan** | a <= b
-- **isInRange** | a <= c && c <= b
-- **isNotInRage** | a > c || c < b
-- **isNegative** | a < 0
-- **isPositive** | a > 0
-- **isEven** | a % 2 == 0
-- **isOdd** | a % 2 != 0
+- **isEqualTo** { a == b }
+- **isNotEqualTo** { a != b }
+- **isBiggerThan** { a > b }
+- **isSmallerThan** { a < b }
+- **isEqualOrBiggerThan** { a >= b }
+- **isEqualOrSmallerThan** { a <= b }
+- **isInRange** { a <= x && x <= b }
+- **isNotInRage** { x < a && b < x }
+- **isNegative** { a < 0 }
+- **isPositive** { a > 0 }
+- **isEven** { x % 2 == 0 }
+- **isOdd** { x % 2 != 0 }
 
+## com.github.puddingspudding.Predicate
+- **then(Runnable)**
+- **orElse(Runnable)**
+- **elseIf(Runnable)**
 
 ## Examples
-
+### java.util.function.Predicate
 ```java
+import java.util.function.Predicate;
 import static com.github.puddingspudding.IntegerPredicates.*;
 import static com.github.puddingspudding.LongPredicates.*;
 
@@ -48,6 +54,23 @@ Stream.of(1,2,3,4,5,6)
 	.filter(isBiggerThan(3))
 	.collect(Collectors.toList());
 	// [4, 5, 6]
+
+```
+### com.github.puddingspudding.Predicate
+```java
+import java.util.function.Predicate;
+import com.github.puddingspudding.Predicates;
+
+Predicate<Integer> isEqualToOne = i -> i == 1;
+
+Predicates.of(isEqualToOne)
+    .then(() -> {
+        // do stuff
+    })
+    .orElse(() -> {
+        // do stuff
+    });
+    .test(1);
 
 ```
 
---
+++ src/main/java/com/github/puddingspudding/Predicate.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+package com.github.puddingspudding;
+
+import java.util.Objects;
+
+/**
+ * Predicate extended with then(), orElse() and elseIf().
+ */
+@FunctionalInterface
+public interface Predicate<T> extends java.util.function.Predicate<T> {
+
+    default Predicate<T> then(Runnable r) {
+        return t -> {
+            if (this.test(t)) {
+                r.run();
+                return true;
+            }
+            return false;
+        };
+    }
+
+    default Predicate<T> elseIf(java.util.function.Predicate<T> p) {
+        return t -> {
+            if (!this.test(t)) {
+                if (p.test(t)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    default Predicate<T> orElse(Runnable r) {
+        return t -> {
+            if (!this.test(t)) {
+                r.run();
+                return false;
+            }
+            return true;
+        };
+    }
+
+    @Override
+    default Predicate<T> and(java.util.function.Predicate<? super T> other) {
+        Objects.requireNonNull(other);
+        return (t) -> test(t) && other.test(t);
+    }
+
+    @Override
+    default Predicate<T> negate() {
+        return (t) -> !test(t);
+    }
+
+    @Override
+    default Predicate<T> or(java.util.function.Predicate<? super T> other) {
+        Objects.requireNonNull(other);
+        return (t) -> test(t) || other.test(t);
+    }
+
+}
